7c6b57e08547c537fd1bb1ef9ba3294599f54b63
nayira <114664058+vd8761@users.noreply.github.com>
2025-11-22 12:50:20 +0530

    Revamp AssessmentRunner UI and stepper logic
    Improves the AssessmentRunner component with a sliding window stepper, enhanced progress indicator, and more questions. Refactors stepper arrow icons for better sizing and color handling, updates DashboardLayout to accept children and navigation props, and polishes option and layout styling for a more modern, responsive experience.

--- components/icons/StepperIcons.tsx
+++ components/icons/StepperIcons.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 // Completed SVG Icon
 export const CompletedStepIcon: React.FC = () => (
   <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 sm:w-10 sm:h-10">
-    <path d="M20 0C8.97243 0 0 8.97243 0 20C0 31.0276 8.97243 40 20 40C31.0276 40 40 31.0276 40 20C40 8.97243 31.0276 0 20 0ZM31.1779 14.7368L18.396 27.4185C17.6441 28.1704 16.4411 28.2206 15.6391 27.4687L8.87218 21.3033C8.07018 20.5514 8.02005 19.2982 8.72181 18.4962C9.47369 17.6942 10.7268 17.6441 11.5288 18.396L16.8922 23.3083L28.3208 11.8797C29.1228 11.0777 30.3759 11.0777 31.1779 11.8797C31.98 12.6817 31.98 13.9348 31.1779 14.7368Z" fill="#1ED36A"/>
+    <path d="M20 0C8.97243 0 0 8.97243 0 20C0 31.0276 8.97243 40 20 40C31.0276 40 40 31.0276 40 20C40 8.97243 31.0276 0 20 0ZM31.1779 14.7368L18.396 27.4185C17.6441 28.1704 16.4411 28.2206 15.6391 27.4185L8.87218 21.3033C8.07018 20.5514 8.02005 19.2982 8.72181 18.4962C9.47369 17.6942 10.7268 17.6441 11.5288 18.396L16.8922 23.3083L28.3208 11.8797C29.1228 11.0777 30.3759 11.0777 31.1779 11.8797C31.98 12.6817 31.98 13.9348 31.1779 14.7368Z" fill="#1ED36A"/>
   </svg>
 );
 
 // Inprogress SVG Icon
 export const CurrentStepIcon: React.FC = () => (
   <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 sm:w-10 sm:h-10">
-    <circle cx="20" cy="20" r="20" fill="white" fillOpacity="0.12" className="fill-brand-light-tertiary dark:fill-white/10"/>
+    <circle cx="20" cy="20" r="20" className="fill-brand-light-tertiary dark:fill-white/10"/>
     <circle cx="20" cy="20" r="4" fill="#1ED36A"/>
   </svg>
 );
@@ -18,6 +18,6 @@ export const CurrentStepIcon: React.FC = () => (
 // Not Started SVG Icon
 export const PendingStepIcon: React.FC = () => (
   <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 sm:w-10 sm:h-10">
-    <circle cx="20" cy="20" r="20" fill="white" fillOpacity="0.12" className="fill-brand-light-tertiary dark:fill-white/10"/>
+    <circle cx="20" cy="20" r="20" className="fill-brand-light-tertiary dark:fill-white/10"/>
   </svg>
 );
